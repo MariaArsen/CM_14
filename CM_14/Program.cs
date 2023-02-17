bd50da4d96f820c67dbac2da4e2574c9ae585eff
Солнце <@LAPTOP-DPKNJGCI>
2023-02-17 13:09:56 +0300

    Добавьте файлы проекта.

---
+++ CM_14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CM_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Джулия");
+            {
+                cat.ShowInfo();
+            }
+            Dog dog = new Dog("Бонд");
+            { 
+                dog.ShowInfo();
+            }
+            Console.ReadKey();
+        }
+    }
+}
